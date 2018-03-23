043d44d73dbd8879195e4e510e1ed5feca2db057
Sara Parks <sparks@synapsesoftware.com>
2018-03-23 15:07:16 -0600

    further functions and query defined

---
+++ functions/bookSuggestions.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+//sql query to db to select first three results from table and spit out
+
+
+ ?>
---
+++ getbooksuggview.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Book Suggestions</title>
+</head>
+<body>
+<h1>Find some book suggestions</h1>
+<button onclick="bookSuggestions">Start Here</button>
+</body>
