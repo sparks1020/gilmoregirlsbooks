da0ac31302dcfe73ed6ddbed3621e05857deca45
Sara Parks <sparks@synapsesoftware.com>
2018-04-05 14:00:42 -0600

    styling updated

--- index.php
+++ index.php
@@ -3,11 +3,17 @@
 <?php include "header.php";?>
 
 <div class="container">
-    <div class"row">
-        <div class="col-md-5 offset-md-3">
-            <a class="btn btn-primary" href="/getBook.php">Find a new book</a>
-            <a class="btn btn-primary" href="/bookStatus.php">Update book status</a>
-            <a class="btn btn-primary" href="/bookAdditions.php">Add a new book</a>
+    <div class="home-row">
+        <div class="row">
+            <div class="col-md-3 offset-md-2">
+                <a class="btn btn-primary" href="/getBook.php">Find a new book</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-primary" href="/bookStatus.php">Update book status</a>
+            </div>
+            <div class="col-md-3">
+                <a class="btn btn-primary" href="/bookAdditions.php">Add a new book</a>
+            </div>
         </div>
     </div>
 </div>
