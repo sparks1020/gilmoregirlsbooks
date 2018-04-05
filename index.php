a14e5cdacb5ca7aae67c866ab3a016889d2b8c2d
Sara Parks <sparks@synapsesoftware.com>
2018-04-05 13:21:45 -0600

    styling home page

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <body>
 <?php include "header.php";?>
 
-<div class"row">
-    <a class"btn btn-primary" href="/getBook.php";>Find a new book</a>
-    <a class"btn btn-primary" href="/bookStatus.php";>Update book status</a>
-    <a class"btn btn-primary" href="/bookAdditions.php";>Add a new book</a>
+<div class="container">
+    <div class"row">
+        <div class="col-md-5 offset-md-3">
+            <a class="btn btn-primary" href="/getBook.php">Find a new book</a>
+            <a class="btn btn-primary" href="/bookStatus.php">Update book status</a>
+            <a class="btn btn-primary" href="/bookAdditions.php">Add a new book</a>
+        </div>
+    </div>
 </div>
 <?php include "footer.php";?>    
 </body>
