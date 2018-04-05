01d1625a7f7deee4566ac4a2688ac70c547cdf91
Sara Parks <sparks@synapsesoftware.com>
2018-04-05 12:51:31 -0600

    creating document dsections for styling

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
+</head> 
+</html>
+
---
+++ index.php
@@ -0,0 +1,16 @@
+
+<body>
+<?php include "header.php";?>
+
+<div class"row">
+    <a class"btn btn-primary" href="/getBook.php";>Find a new book</a>
+    <a class"btn btn-primary" href="/bookStatus.php";>Update book status</a>
+    <a class"btn btn-primary" href="/bookAdditions.php";>Add a new book</a>
+</div>
+<?php include "footer.php";?>    
+</body>
+
+
+
+
+
